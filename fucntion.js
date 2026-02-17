502095646f471da8a2e6104eaacf1c7c62c4b038
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-02-17 09:02:41 +0100

    add object storage, skeleton for render

--- fucntion.js
+++ fucntion.js
@@ -2,18 +2,18 @@ const popUp = document.getElementById("open-form");
 const popupWindow = document.getElementById("popup");
 const close = document.getElementById("close");
 const userForm = document.getElementById("userForm");
-const Checkbox = document.getElementById("finished");
+const finishedCheckbox = document.getElementById("finished");
 const currPageDiv = document.getElementById("inputCurrPage");
+const currPageInput = document.getElementById("currPage");
 const myLibrary = [];
-const book = {};
 
 popUp.addEventListener("click", (openPopup));
 
 close.addEventListener("click", (closePopup)); 
 
-Checkbox.addEventListener("change", (updateCurrentPageVisibility));
-
 userForm.addEventListener("submit", createBook);
+
+finishedCheckbox.addEventListener("change", toggleCurrentPage);
 
 function openPopup() {
     popupWindow.classList.remove("hidden");
@@ -23,18 +23,51 @@ function openPopup() {
 function closePopup() {
     popupWindow.classList.add("hidden");
     popup.style.display = "";
+    userForm.reset();
+    toggleCurrentPage()
 };
 
-function updateCurrentPageVisibility(isFinished) {
-  currPageDiv.style.display = isFinished ? "none" : "grid";
+function toggleCurrentPage() {
+    const checked = finishedCheckbox.checked;
+    currPageDiv.style.display = checked ? "none" : "block";
+    currPageInput.disabled = checked;
+
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function createBookObject(form) {
+    const data = new FormData(form);
+
+    return {
+        id: crypto.randomUUID(),
+        author: data.get("author"),
+        title: data.get("title"),
+        pages: data.get("pages"),
+        currPage: data.get("currPage"),
+        finished: data.get("finished") === "on",
+    };
 };
 
 function createBook(e) {
     e.preventDefault();
+
     const data = new FormData(userForm);
-    const book = Object.fromEntries(data);
-    console.log(book);
+    const book = createBookObject(userForm)
+    addBookToLibrary(book)
+    closePopup()
+    userForm.reset();
+    toggleCurrentPage()
 };
 
- 
+function renderBook(data) {
+    const book = "";
+
+    document.getElementById("library").innerHTML += book
+
+};
+    
+
 
